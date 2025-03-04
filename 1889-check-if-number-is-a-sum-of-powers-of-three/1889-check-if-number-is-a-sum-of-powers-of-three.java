deff588fe868714e0bce22cca293b02bc50a18ee
ishan1603 <ishanm1603@gmail.com>
2025-03-04 11:35:26 +0530

    Time: 0 ms (100%), Space: 40.6 MB (43.9%) - LeetHub

--- 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.java
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.java
@@ -1,27 +1,15 @@
 class Solution {
 
     public boolean checkPowersOfThree(int n) {
-        int power = 0;
-
-        // Find the largest power that is smaller or equal to n
-        while (Math.pow(3, power) <= n) {
-            power++;
-        }
-
         while (n > 0) {
-            // Subtract current power from n
-            if (n >= Math.pow(3, power)) {
-                n -= (int) Math.pow(3, power);
-            }
-            // We cannot use the same power twice
-            if (n >= Math.pow(3, power)) {
+            // Check if this power should be used twice
+            if (n % 3 == 2) {
                 return false;
             }
-            // Move to the next lower power
-            power--;
+            // Divide n by 3 to move to the next greater power
+            n /= 3;
         }
-
-        // n has reached 0
+        // The ternary representation of n consists only of 0s and 1s
         return true;
     }
 }
